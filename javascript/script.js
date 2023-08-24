acb55060dbe472748f9aa5fda9f92719fc242948
Samuel Yorke Aidoo <sya.aidoo@gmail.com>
2023-08-24 16:03:31 +0200

    Create simple functions for add, subtract, multiply and divide

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+
+function add(number1, number2){
+    return number1 + number2;
+
+}
+
+function subtract(number1, number2){
+    return number1 - number2;
+}
+
+function multiply(number1, number2){
+    return number1*number2;
+}
+
+function divide(numerator, denominator){
+    return numerator/denominator;
+
+}
+
+//console.log(divide(5,5));
