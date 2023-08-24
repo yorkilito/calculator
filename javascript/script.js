f9370d122d58697e6afc817df902ab804fb280dd
Samuel Yorke Aidoo <sya.aidoo@gmail.com>
2023-08-24 16:22:58 +0200

    Create operate function

--- javascript/script.js
+++ javascript/script.js
@@ -12,9 +12,33 @@ function multiply(number1, number2){
     return number1*number2;
 }
 
-function divide(numerator, denominator){
-    return numerator/denominator;
+function divide(number1, number2){
+    return number1/number2;
 
 }
 
-//console.log(divide(5,5));
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(operator, number1, number2){
+    switch(operator){
+        case "+":
+            return add(number1,number2);
+            break;
+        case "-":
+            return subtract(number1,number2);
+            break;
+        case "*":
+            return multiply(number1,number2);
+            break;
+        case "/":
+            return divide(number1,number2);
+            break;
+        default:
+            return undefined;
+    }
+
+}
+
+console.log(operate("/",15,5));
